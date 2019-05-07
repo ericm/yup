647f9b420b9e8b2604127099aea545084a41fc28
Eric Moynihan <git@moynihan.io>
2019-05-07 17:37:06 +0100

    Added file checks, configs and VSCODE tasks

--- main.go
+++ main.go
@@ -9,6 +9,16 @@ import (
 
 const configFileName string = "yay.conf"
 
+// Global variables
+var (
+	configDir string
+	cacheDir  string
+
+	configFile string
+)
+
+// Entrypoint to the program.
+// Runs intitialization and calls other packages.
 func main() {
 	if os.Geteuid() == 0 {
 		fmt.Fprintln(os.Stderr, "No need to run yup as sudo/root")
@@ -17,10 +27,13 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Requires linux I'm afraid")
 		os.Exit(1)
 	}
-	exitError(paths())
 
+	exitError(paths())
+	exitError(makePaths())
+	exitError(initConfig())
 }
 
+// Exits the program upon a core function returning an error.
 func exitError(err error) {
 	if err != nil {
 		if str := err.Error(); str != "" {
@@ -30,14 +43,53 @@ func exitError(err error) {
 	}
 }
 
+// Sets global path variables
 func paths() error {
-	configDir := os.Getenv("HOME")
-	cacheDir := configDir
-	filepath.Join(configDir, ".config/yup")
-	filepath.Join(cacheDir, ".cache/yup")
+	configDir = os.Getenv("HOME")
+	if len(configDir) == 0 {
+		return fmt.Errorf("HOME environment variable unset")
+	}
+
+	cacheDir = filepath.Join(configDir, ".cache/yup")
+	configDir = filepath.Join(configDir, ".config/yup")
+
+	configFile = filepath.Join(configDir, configFileName)
 	return nil
 }
 
-func makeConfig() {
+// Checks and creates the directories and file required by yup.
+func makePaths() error {
+	// Config
+	if _, err := os.Stat(configDir); os.IsNotExist(err) {
+		if err = os.MkdirAll(configDir, 0755); err != nil {
+			fmt.Println(len(configDir))
+			return fmt.Errorf("Failed to create config directory: %s", err)
+		}
+		// Create file
+		os.OpenFile(configFile, os.O_CREATE, 0664)
+	} else if err != nil {
+		return err
+	}
 
+	// Cache
+	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+		if err = os.MkdirAll(cacheDir, 0755); err != nil {
+			return fmt.Errorf("Failed to create cache directory: %s", err)
+		}
+	} else if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initConfig() error {
+	file, err := os.Open(configDir)
+
+	if !os.IsNotExist(err) && err != nil {
+		return fmt.Errorf("Failed to open config file '%s': %s", file, err)
+	}
+
+	defer file.Close()
+	return nil
 }
