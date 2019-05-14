d34f85e1e0e7b5a80892a00e0a5792cbbde065eb
Eric Moynihan <git@moynihan.io>
2019-05-14 16:36:51 +0100

    Added multithreaded AUR  downloading

--- sync/sync.go
+++ sync/sync.go
@@ -2,6 +2,10 @@ package sync
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/ericm/yup/config"
 	"github.com/mikkeloscar/aur"
@@ -12,6 +16,31 @@ import (
 // 	return nil
 // }
 
+// Download wrapper for io.Reader
+type Download struct {
+	io.Reader
+	total    int64
+	length   int64
+	progress float64
+}
+
+// Read will override io.Reader's Read method
+//
+// Source: https://stackoverflow.com/questions/25645363/track-and-show-downloading-file-summary-in-percentage-go-lang#25645804
+func (dl *Download) Read(p []byte) (int, error) {
+	num, err := dl.Reader.Read(p)
+	if num > 0 {
+		dl.total += int64(num)
+		percentage := float64(dl.total) / float64(dl.length) * float64(100)
+		perInt := int(percentage / float64(10))
+		out := fmt.Sprintf("%v", perInt)
+		if percentage-dl.progress > 2 {
+			fmt.Fprintf(os.Stderr, out)
+		}
+	}
+	return num, err
+}
+
 // Sync from the AUR first, then other configured repos.
 //
 // This checks each package param individually
@@ -19,14 +48,16 @@ func Sync(packages []string) error {
 	// TODO: Check with config
 	errChannel := make(chan error, len(packages))
 	for _, p := range packages {
-		repo, err := aur.Info([]string{p})
-		if err != nil {
-			errChannel <- err
-		}
-		if len(repo) > 0 {
-			// Multithreaded downloads
-			go aurDload("https://aur.archlinux.org"+repo[0].URLPath, errChannel)
-		}
+		// Multithreaded downloads
+		go func(p string) {
+			repo, err := aur.Info([]string{p})
+			if err != nil {
+				errChannel <- err
+			}
+			if len(repo) > 0 {
+				aurDload("https://aur.archlinux.org"+repo[0].URLPath, repo[0].Name+repo[0].Version+".tar.gz", errChannel)
+			}
+		}(p)
 	}
 
 	for _i := 0; _i < len(packages); _i++ {
@@ -40,8 +71,29 @@ func Sync(packages []string) error {
 }
 
 // Download an AUR package to cache
-func aurDload(url string, err chan error) {
+func aurDload(url string, fileName string, errChannel chan error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		errChannel <- err
+		return
+	}
+	defer resp.Body.Close()
+
 	conf := config.GetConfig()
-	fmt.Println(conf.CacheDir)
-	err <- nil
+	file := filepath.Join(conf.CacheDir, fileName)
+
+	out, err := os.Create(file)
+	if err != nil {
+		errChannel <- err
+		return
+	}
+	defer out.Close()
+
+	_, errC := io.Copy(out, resp.Body)
+	if errC != nil {
+		errChannel <- errC
+		return
+	}
+
+	errChannel <- nil
 }
