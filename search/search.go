515344cd8f28ad3a0089555bc49267ee899e4ce9
Eric Moynihan <git@moynihan.io>
2019-06-02 14:27:59 +0100

    Began Pacman Search

--- search/search.go
+++ search/search.go
@@ -1,5 +1,11 @@
 package search
 
+import (
+	"os/exec"
+
+	"github.com/ericm/yup/output"
+)
+
 // Package represents a package in pacman or the AUR
 type Package struct {
 	Aur              bool
@@ -12,4 +18,15 @@ type Package struct {
 	InstalledVersion string
 	InstalledSize    int64
 	SortValue        float64
+}
+
+// Pacman returns []Package parsed from pacman
+func Pacman(query string) []Package {
+	search := exec.Command("pacman", "-Ss", query)
+	output.SetStd(search)
+
+	var packs []Package
+	// TODO: parse search
+
+	return packs
 }
