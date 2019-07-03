94424bf85bde50e232750db6cacfb109ddf77347
Eric Moynihan <git@moynihan.io>
2019-07-03 17:43:03 +0100

    Added parser to check for AUR updates

--- update/update.go
+++ update/update.go
@@ -3,7 +3,14 @@ package update
 import (
 	"github.com/ericm/yup/output"
 	"os/exec"
+	"strings"
 )
+
+// Installed Packages representation
+type installedPack struct {
+	name,
+	version string
+}
 
 // Update runs system update from repos
 func Update() error {
@@ -20,6 +27,21 @@ func Update() error {
 
 // AurUpdate checks for update in the AUR
 func AurUpdate() error {
+	// Filter installed packages
+	packs := []installedPack{}
+
+	// Get output of pacman -Q
+	cmd := exec.Command("pacman", "-Q")
+	inp, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	packStr := strings.Split(string(inp), "\n")
+	for _, pack := range packStr {
+		p := strings.Split(pack, " ")
+		packs = append(packs, installedPack{p[0], p[1]})
+	}
 
 	return nil
 }
