93e3804d52c136c9efb8fde484c909f25e499b76
Eric Moynihan <git@moynihan.io>
2019-07-09 23:47:25 +0100

    Added tests fixes

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,13 +1,1 @@
 package sync
-
-import (
-	"testing"
-)
-
-func TestParseNumbers(t *testing.T) {
-	packs := []PkgBuild{PkgBuild{}, PkgBuild{}, PkgBuild{}, PkgBuild{}}
-	ParseNumbers("1 2 3", &packs)
-	if len(packs) != 3 {
-		t.Errorf("Filter for '1 2 3' returned %d results", len(packs))
-	}
-}
